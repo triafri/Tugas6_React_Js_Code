8f91c5752f0f823af78f16c303839089c936cbf7
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-01 22:20:46 +0700

    Add files via upload

---
+++ src/Lib/DaftarMinuman.js
@@ -0,0 +1,29 @@
+const DaftarMinuman=[
+    {
+        minuman:'Es Cendol',
+        harga:'10.000',
+        img:'https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-cendol.jpg'
+    },
+    {
+        minuman:'Es Pisang Ijo',
+        harga:'10.000',
+        img:'https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pisang-hijau.jpg'
+    },
+    {
+        minuman:'Es Kacang Merah',
+        harga:'10.000',
+        img:'https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-kacang-merah.jpg'
+    },
+    {
+        minuman:'Es Pleret',
+        harga:'10.000',
+        img:'https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pleret.jpg'
+    },
+    {
+        minuman:'Es Legen',
+        harga:'15.000',
+        img:'https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-legen.jpg'
+    }
+]
+
+export default DaftarMinuman
---
+++ src/Page/MenuMinuman.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import DaftarMinuman from '../Lib/DaftarMinuman';
+import Header from './Header';
+
+class MenuMinuman extends Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <h3>Daftar Minuman Favourite</h3>
+                <table style={{width:'100%'}}>
+                    <tbody>
+                        <tr>
+                            {DaftarMinuman.map((data, index)=>{
+                                return(
+                                    <td key={index}>
+                                        <center>
+                                            <img src={data.img}
+                                            alt='Product Minuman'
+                                            width='150'
+                                            height='100'
+                                            />
+                                            <p>{data.minuman}</p>
+                                            <p>Harga : Rp. {data.harga}</p>
+                                        </center>
+                                    </td>
+                                )
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default MenuMinuman
